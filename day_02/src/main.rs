dc08ded2a84660e47b60b5d74325b757373e7bc3
Ryan Kent <ryan@interphased.com>
2023-12-02 09:35:30 -0500

    day 1

---
+++ day_02/src/main.rs
@@ -0,0 +1,15 @@
+mod tests;
+
+fn main() {
+    let input = include_str!("input.txt");
+    let part1 = part1(input);
+    let part2 = part2(input);
+    dbg!(part1);
+    dbg!(part2);
+}
+
+fn part1(input: &str) -> String {
+}
+
+fn part2(input: &str) -> String {
+}
---
+++ day_02/src/tests.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod tests {
+    use crate::{part1, part2};
+
+    // #[test]
+    // fn test_1() {
+    //     let result = part1("1abc2");
+    //     assert_eq!(result, "12");
+    // }
+}
