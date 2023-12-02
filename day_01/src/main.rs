e69c0938e61d2cd35e4edb07ce4307baf5fb453e
Ryan Kent <ryan@interphased.com>
2023-12-02 12:39:00 -0500

    day 2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,107 +1,110 @@
 mod tests;
 
 fn main() {
-    let input = include_str!("input.txt");
-    let part1 = part1(input);
-    let part2 = part2(input);
-    dbg!(part1);
-    dbg!(part2);
+	let input = include_str!("input.txt");
+	let part1 = part1(input);
+	let part2 = part2(input);
+	dbg!(part1);
+	dbg!(part2);
 }
 
 fn part1(input: &str) -> String {
-    let mut total = 0;
+	let mut total = 0;
 
-    for line in input.lines() {
-        let mut first_char = None;
-        let mut last_char = None;
+	for line in input.lines() {
+		let mut first_char = None;
+		let mut last_char = None;
 
-        for char in line.chars() {
-            if char.is_numeric() {
-                first_char = Some(char);
-                break;
-            }
-        }
+		for char in line.chars() {
+			if char.is_numeric() {
+				first_char = Some(char);
+				break;
+			}
+		}
 
-        for char in line.chars().rev() {
-            if char.is_numeric() {
-                last_char = Some(char);
-                break;
-            }
-        }
+		for char in line.chars().rev() {
+			if char.is_numeric() {
+				last_char = Some(char);
+				break;
+			}
+		}
 
-        if first_char.is_some() && last_char.is_some() {
-            let amount = format!("{}{}", first_char.unwrap(), last_char.unwrap());
-            total += amount.parse::<i32>().unwrap();
-        }
-    }
+		if first_char.is_some() && last_char.is_some() {
+			let amount = format!("{}{}", first_char.unwrap(), last_char.unwrap());
+			total += amount.parse::<i32>().unwrap();
+		}
+	}
 
-    total.to_string()
+	total.to_string()
 }
 
 fn part2(input: &str) -> String {
-    let mut total = 0;
+	let mut total = 0;
 
-    for line in input.lines() {
-        let mut first_char: Option<(usize, char)> = None;
-        let mut last_char: Option<(usize, char)> = None;
+	for line in input.lines() {
+		let mut first_char: Option<(usize, char)> = None;
+		let mut last_char: Option<(usize, char)> = None;
 
-        let words = vec![
-            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-        ];
+		let words = vec![
+			"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+		];
 
-        for (word_index, word) in words.iter().enumerate() {
-            let forwards = line.find(word);
-            match forwards {
-                Some(index) => {
-                    let fc = char::from_digit(word_index as u32, 10).unwrap();
+		for (word_index, word) in words.iter().enumerate() {
+			let forwards = line.find(word);
+			match forwards {
+				Some(index) => {
+					let fc = char::from_digit(word_index as u32, 10).unwrap();
 
-                    if first_char.is_none() || (first_char.is_some() && first_char.unwrap().0 > index) {
-                        first_char = Some((index, fc));
-                    }
-                }
-                None => ()
-            }
+					if first_char.is_none()
+						|| (first_char.is_some() && first_char.unwrap().0 > index)
+					{
+						first_char = Some((index, fc));
+					}
+				}
+				None => (),
+			}
 
-            let backwards = line.rfind(word);
-            match backwards {
-                Some(index) => {
-                    let lc = char::from_digit(word_index as u32, 10).unwrap();
+			let backwards = line.rfind(word);
+			match backwards {
+				Some(index) => {
+					let lc = char::from_digit(word_index as u32, 10).unwrap();
 
-                    if last_char.is_none() || (last_char.is_some() && last_char.unwrap().0 < index) {
-                        last_char = Some((index, lc));
-                    }
-                }
-                None => (),
-            }
-        }
+					if last_char.is_none() || (last_char.is_some() && last_char.unwrap().0 < index)
+					{
+						last_char = Some((index, lc));
+					}
+				}
+				None => (),
+			}
+		}
 
-        for (char_index, char) in line.chars().enumerate() {
-            if char.is_numeric() {
-                match first_char {
-                    Some(_) => {
-                        if char_index < first_char.unwrap().0 {
-                            first_char = Some((char_index, char));
-                        }
-                    }
-                    None => first_char = Some((char_index, char))
-                }
+		for (char_index, char) in line.chars().enumerate() {
+			if char.is_numeric() {
+				match first_char {
+					Some(_) => {
+						if char_index < first_char.unwrap().0 {
+							first_char = Some((char_index, char));
+						}
+					}
+					None => first_char = Some((char_index, char)),
+				}
 
-                match last_char {
-                    Some(_) => {
-                        if char_index > last_char.unwrap().0 {
-                            last_char = Some((char_index, char));
-                        }
-                    }
-                    None => last_char = Some((char_index, char))
-                }
-            }
-        }
+				match last_char {
+					Some(_) => {
+						if char_index > last_char.unwrap().0 {
+							last_char = Some((char_index, char));
+						}
+					}
+					None => last_char = Some((char_index, char)),
+				}
+			}
+		}
 
-        if first_char.is_some() && last_char.is_some() {
-            let amount = format!("{}{}", first_char.unwrap().1, last_char.unwrap().1);
-            total += amount.parse::<i32>().unwrap();
-        }
-    }
+		if first_char.is_some() && last_char.is_some() {
+			let amount = format!("{}{}", first_char.unwrap().1, last_char.unwrap().1);
+			total += amount.parse::<i32>().unwrap();
+		}
+	}
 
-    total.to_string()
+	total.to_string()
 }
--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,15 +1,142 @@
 mod tests;
 
 fn main() {
-    let input = include_str!("input.txt");
-    let part1 = part1(input);
-    let part2 = part2(input);
-    dbg!(part1);
-    dbg!(part2);
+	let input = include_str!("input.txt");
+	let part1 = part1(input);
+	let part2 = part2(input);
+	dbg!(part1);
+	dbg!(part2);
 }
 
-fn part1(input: &str) -> String {
+#[derive(Debug)]
+struct Game {
+	id: i32,
+	possible: bool,
+	power: i32,
 }
 
-fn part2(input: &str) -> String {
+#[derive(Debug)]
+struct Round {
+	red: i32,
+	green: i32,
+	blue: i32,
+}
+
+fn part1(input: &str) -> i32 {
+	let mut total = 0;
+	let max_red = 12;
+	let max_green = 13;
+	let max_blue = 14;
+
+	for line in input.lines() {
+		let split: Vec<&str> = line.split(": ").collect();
+
+		let mut game = Game {
+			id: split[0].replace("Game ", "").parse::<i32>().unwrap(),
+			possible: true,
+			power: 0,
+		};
+
+		let rounds: Vec<&str> = split[1].split("; ").collect();
+
+		for r in rounds {
+			let colours: Vec<&str> = r.split(", ").clone().collect();
+
+			let mut round = Round {
+				red: 0,
+				green: 0,
+				blue: 0,
+			};
+
+			for colour in colours {
+				if colour.contains("red") {
+					round.red = colour.split(" red").next().unwrap().parse::<i32>().unwrap();
+				}
+				if colour.contains("green") {
+					round.green = colour
+						.split(" green")
+						.next()
+						.unwrap()
+						.parse::<i32>()
+						.unwrap();
+				}
+				if colour.contains("blue") {
+					round.blue = colour
+						.split(" blue")
+						.next()
+						.unwrap()
+						.parse::<i32>()
+						.unwrap();
+				}
+			}
+
+			if round.red > max_red || round.blue > max_blue || round.green > max_green {
+				game.possible = false;
+			}
+		}
+
+		if game.possible {
+			total += game.id;
+		}
+	}
+
+	total
+}
+
+fn part2(input: &str) -> i32 {
+	let mut total = 0;
+
+	for line in input.lines() {
+		let split: Vec<&str> = line.split(": ").collect();
+
+		let mut game = Game {
+			id: split[0].replace("Game ", "").parse::<i32>().unwrap(),
+			possible: true,
+			power: 0,
+		};
+
+		let mut reds: Vec<i32> = vec![];
+		let mut greens: Vec<i32> = vec![];
+		let mut blues: Vec<i32> = vec![];
+
+		let rounds_vec: Vec<&str> = split[1].split("; ").collect();
+
+		for r in rounds_vec {
+			let colours: Vec<&str> = r.split(", ").clone().collect();
+
+			for colour in colours {
+				if colour.contains("red") {
+					reds.push(colour.split(" red").next().unwrap().parse::<i32>().unwrap());
+				}
+				if colour.contains("green") {
+					greens.push(
+						colour
+							.split(" green")
+							.next()
+							.unwrap()
+							.parse::<i32>()
+							.unwrap(),
+					);
+				}
+				if colour.contains("blue") {
+					blues.push(
+						colour
+							.split(" blue")
+							.next()
+							.unwrap()
+							.parse::<i32>()
+							.unwrap(),
+					);
+				}
+			}
+		}
+
+		game.power = reds.into_iter().reduce(i32::max).unwrap()
+			* greens.into_iter().reduce(i32::max).unwrap()
+			* blues.into_iter().reduce(i32::max).unwrap();
+
+		total += game.power;
+	}
+
+	total
 }
