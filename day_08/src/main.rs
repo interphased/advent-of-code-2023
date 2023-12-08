2ffb9792a99ab18cae52cf331b7934f9b9f34e02
Ryan Kent <ryan@interphased.com>
2023-12-08 17:52:31 -0500

    day 8 part 2

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -15,17 +15,9 @@ fn part1(input: &str) -> i32 {
     let mut map: HashMap<String, (String, String)> = HashMap::new();
 
     input.lines().skip(2).for_each(|l| {
-        let address = l.split_ascii_whitespace().nth(0).unwrap().to_string();
-        let left = l
-            .split_ascii_whitespace()
-            .nth(2)
-            .unwrap()
-            .chars()
-            .skip(1)
-            .take(3)
-            .collect::<String>();
-        let mut right = l.split_ascii_whitespace().nth(3).unwrap().to_string();
-        right.pop();
+        let address = l.chars().take(3).collect::<String>();
+        let left = l.chars().skip(7).take(3).collect::<String>();
+        let right = l.chars().skip(12).take(3).collect::<String>();
 
         map.insert(address, (left, right));
     });
@@ -58,6 +50,69 @@ fn part1(input: &str) -> i32 {
     steps
 }
 
-fn part2(input: &str) -> usize {
-    0
+fn part2(input: &str) -> i64 {
+    let directions: Vec<char> = input.lines().nth(0).unwrap().chars().collect();
+    let mut map: HashMap<String, (String, String)> = HashMap::new();
+
+    input.lines().skip(2).for_each(|l| {
+        let address = l.chars().take(3).collect::<String>();
+        let left = l.chars().skip(7).take(3).collect::<String>();
+        let right = l.chars().skip(12).take(3).collect::<String>();
+
+        map.insert(address, (left, right));
+    });
+
+    let mut steps_vec: Vec<i64> = vec![];
+
+    let starts: Vec<String> = map
+        .iter()
+        .filter(|m| m.0.ends_with("A"))
+        .map(|m| m.0.to_string())
+        .collect();
+
+    let ends: Vec<String> = map
+        .iter()
+        .filter(|m| m.0.ends_with("Z"))
+        .map(|m| m.0.to_string())
+        .collect();
+
+    let mut founds: HashMap<String, bool> = HashMap::new();
+
+    for start in starts {
+        let mut steps = 0;
+        let found = false;
+        let mut current = start.clone();
+
+        'outer: while !found {
+            for &direction in &directions {
+                steps += 1;
+                let node = map.get(&current).expect("Start node not found");
+
+                if direction == 'L' {
+                    if ends.contains(&node.0) {
+                        founds.insert(node.0.clone(), true);
+                        break 'outer;
+                    }
+                    current = node.0.clone();
+                }
+                if direction == 'R' {
+                    if ends.contains(&node.1) {
+                        founds.insert(node.0.clone(), true);
+                        break 'outer;
+                    }
+                    current = node.1.clone();
+                }
+                founds.insert(node.0.clone(), false);
+            }
+        }
+
+        steps_vec.push(steps);
+    }
+
+    let mut total: i64 = steps_vec[0];
+    for i in 0..steps_vec.iter().count() {
+        total = num::integer::lcm(total, steps_vec[i]);
+    }
+
+    total
 }
--- day_08/src/tests.rs
+++ day_08/src/tests.rs
@@ -29,4 +29,21 @@ ZZZ = (ZZZ, ZZZ)",
         );
         assert_eq!(result, 6);
     }
+
+    #[test]
+    fn test_2_1() {
+        let result = part2(
+            "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)",
+        );
+        assert_eq!(result, 6);
+    }
 }
