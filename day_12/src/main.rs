2b48407c512c1bbdde760f4eefd637229e9f2bd6
Ryan Kent <ryan@interphased.com>
2023-12-12 19:18:22 -0500

    setup day 12

---
+++ day_12/src/main.rs
@@ -0,0 +1,19 @@
+use array2d::Array2D;
+
+mod tests;
+
+fn main() {
+    let input = include_str!("input.txt");
+    let part1 = part1(input);
+    let part2 = part2(input);
+    dbg!(part1);
+    dbg!(part2);
+}
+
+fn part1(input: &str) -> i32 {
+    0
+}
+
+fn part2(input: &str) -> i64 {
+    0
+}
